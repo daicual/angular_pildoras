48d23d93aea5f48a7477dc18242211c9ffee0f68
Darío Aícua Ubienra <dariolaredo@gmail.com>
2023-04-01 19:20:14 +0200

    Directivas II 15

--- trabajoDirectivaApp/src/app/app.component.ts
+++ trabajoDirectivaApp/src/app/app.component.ts
@@ -11,6 +11,19 @@ export class AppComponent {
   registrado=false;
   nombre ="";
   apellido ="";
+  entradas:Array<{titulo:string}>;
+
+  constructor(){
+    this.entradas=[
+      {titulo:"Python, cada día más presente"},
+      {titulo:"Java, presente desde hace más de 20 años"},
+      {titulo:"JavaScript, cada vez más funcional"},
+      {titulo:"Kotlin, potencia para tus apps"},
+      {titulo:"¿Dónde quedó Pascal?"},
+      
+    ]
+  }
+
   registrarUsuario(){
     this.registrado = true;
     this.mensaje = "Usuario registrado con exito con el nombre de "+this.nombre+" "+this.apellido;
